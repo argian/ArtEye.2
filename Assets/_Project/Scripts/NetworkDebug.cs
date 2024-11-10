29b8d354e42af1cc786e3a67e0c87beea261adbc
Michał Biernat <44023124+m-biernat@users.noreply.github.com>
2024-11-11 00:31:28 +0100

    Add NetworkDebug in Editor

---
+++ Assets/_Project/Scripts/NetworkDebug.cs
@@ -0,0 +1,47 @@
+using Unity.Netcode;
+using UnityEngine;
+
+namespace ArtEye
+{
+    public class NetworkDebug : MonoBehaviour
+    {
+        [SerializeField] private GameObject startHostButton;
+        [SerializeField] private GameObject startClientButton;
+        
+        [Space]        
+        [SerializeField] private GameObject disconnectButton;
+
+        public void StartHost()
+        {
+            NetworkManager.Singleton.StartHost();
+            ToggleButtons();
+        }
+
+        public void StartClient()
+        {
+            NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectCallback;
+            NetworkManager.Singleton.StartClient();
+            ToggleButtons();
+        }
+
+        private void OnClientDisconnectCallback(ulong obj)
+        {
+            ToggleButtons();
+        }
+
+        public void Disconnect()
+        {
+            NetworkManager.Singleton.OnClientDisconnectCallback -= OnClientDisconnectCallback;
+            NetworkManager.Singleton.Shutdown();
+            ToggleButtons();
+        }
+
+        private void ToggleButtons()
+        {
+            startHostButton.SetActive(!startHostButton.activeSelf);
+            startClientButton.SetActive(!startClientButton.activeSelf);
+
+            disconnectButton.SetActive(!disconnectButton.activeSelf);
+        }
+    }
+}
