0559b246e412ad26f97dd1d3a3096863aa2c14d4
Kimel-PK <57668948+Kimel-PK@users.noreply.github.com>
2024-11-26 15:32:25 +0100

    Teleportation code cleanup

--- Assets/_Project/Content/General/Scripts/Teleporters/TeleporterBase.cs
+++ Assets/_Project/Content/General/Scripts/Teleporters/TeleporterBase.cs
@@ -12,31 +12,36 @@ namespace ArtEye.Teleportation
         private static bool TeleportationInProgress { get; set; }
         
         [SerializeField] protected XRBaseInteractable xrGrabInteractable;
-
-        private Sequence sequence;
         
         protected void OnEnable() => xrGrabInteractable.selectEntered.AddListener(OnGrab);
 
         protected void OnDisable() => xrGrabInteractable.selectEntered.RemoveListener(OnGrab);
-
-        private void OnGrab(SelectEnterEventArgs args) {
-            if (TeleportationInProgress)
-                return;
-
-            GameObject playerObject = GetLocalPlayerObject(args);
-            
-            sequence = DOTween.Sequence();
-            sequence.AppendCallback(() => { TeleportationInProgress = true; });
-            // TODO Fade out
-            sequence.Append(DOTween.To(() => 1f, value => { Debug.Log($"Fade: {value}"); }, 1f, 1f).SetEase(Ease.Linear));
-            sequence.AppendCallback(async () => await Teleport(playerObject));
-            // TODO Fade in
-            sequence.Append(DOTween.To(() => 1f, value => { Debug.Log($"Fade: {value}"); }, 0f, 1f).SetEase(Ease.Linear));
-            sequence.AppendCallback(() => { TeleportationInProgress = false; });
-        }
         
         protected abstract UniTask Teleport(GameObject playerObject);
 
-        private GameObject GetLocalPlayerObject(SelectEnterEventArgs args) => args.interactorObject.transform.GetComponentInParent<XROrigin>().gameObject;
+        private void OnGrab(SelectEnterEventArgs args)
+        {
+            if (TeleportationInProgress)
+                return;
+            
+            Teleportation(GetLocalPlayerObject(args)).Forget();
+        }
+
+        private async UniTaskVoid Teleportation(GameObject playerObject)
+        {
+            TeleportationInProgress = true;
+            // TODO Fade out
+            // await playerObject.FadeOut();
+            await Teleport(playerObject);
+            // TODO Fade in
+            // await playerObject.FadeIn();
+            TeleportationInProgress = false;
+        }
+
+        private GameObject GetLocalPlayerObject(SelectEnterEventArgs args)
+        {
+            // TODO access local player or find something like Player component in hierarchy
+            return args.interactorObject.transform.GetComponentInParent<XROrigin>().gameObject;
+        }
     }
 }
