52f9d8a70484687e54ff51913264705bf959d55f
Michał Biernat <44023124+m-biernat@users.noreply.github.com>
2024-12-23 01:20:28 +0100

    Add spatial menu repositioning

--- Assets/_Project/Scripts/UI/SpatialMenu.cs
+++ Assets/_Project/Scripts/UI/SpatialMenu.cs
@@ -8,12 +8,31 @@ namespace ArtEye
         [SerializeField] private InputActionReference menuActionRef;
         [SerializeField] private ActionTriggerSO menuActionTrigger;
 
+        [Space]
+        [SerializeField] private Vector3 offset = new(0, .2f, 1.25f);
+
+        Transform hmdTransform;
+
         private void Awake()
         {
             menuActionRef.action.performed += TriggerMenu;
             menuActionTrigger.Performed += TriggerMenu;
-            
+
+            hmdTransform = Camera.main.transform;
+
             gameObject.SetActive(false);
+        }
+
+        private void OnEnable() => Reposition();
+
+        private void Reposition()
+        {
+            Vector3 newPosition = hmdTransform.position + (hmdTransform.forward * offset.z);
+            newPosition.x += offset.x;
+            newPosition.y *= .5f;
+            newPosition.y += offset.y;
+
+            transform.position = newPosition;
         }
 
         private void TriggerMenu(InputAction.CallbackContext ctx) => TriggerMenu();
