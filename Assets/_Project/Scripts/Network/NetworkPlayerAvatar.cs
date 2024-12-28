dc5087cf6baff45b7d456941af9db14abe1919ff
Michał Biernat <44023124+m-biernat@users.noreply.github.com>
2024-12-28 18:29:21 +0100

    Add random color on local player setup

--- Assets/_Project/Scripts/Network/NetworkPlayerAvatar.cs
+++ Assets/_Project/Scripts/Network/NetworkPlayerAvatar.cs
@@ -5,11 +5,18 @@ namespace ArtEye
 {
     public class NetworkPlayerAvatar : XRINetworkPlayer
     {
+        private static Color? _localPlayerColor = null; // Not great but it works.
+
         protected override void SetupLocalPlayer()
         {
             base.SetupLocalPlayer();
 
             UpdateLocalPlayerName(PlayerPrefs.GetString("Name"));
+
+            if (_localPlayerColor == null)
+                _localPlayerColor = Random.ColorHSV(0, 1, .8f, .8f, .8f, .8f);
+            
+            UpdateLocalPlayerColor(_localPlayerColor.Value);
         }
 
         public override void OnNetworkSpawn()
